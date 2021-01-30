aa4b8a3957eefb302afe611b0de5d7bdde90f404
Lais Leal <laisdina@id.uff.br>
2021-01-30 10:54:49 -0300

    Implementação da classe de domínio Terminal e retorno desses objetos pelo método de requisição GET.

---
+++ src/main/java/com/laisleal/desafiotecnico/domain/Terminal.java
@@ -0,0 +1,141 @@
+package com.laisleal.desafiotecnico.domain;
+
+import java.io.Serializable;
+
+public class Terminal implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private Integer logic;
+	private String serial;
+	private String model;
+	private Integer sam;
+	private String ptid;
+	private Integer plat;
+	private String version;
+	private Integer mxr;
+	private Integer mxf;
+	private String VERFM;
+	
+	public Terminal() {
+	}
+
+	public Terminal(Integer logic, String serial, String model, Integer sam, String ptid, Integer plat, String version, Integer mxr, Integer mxf, String VERFM) {
+		super();
+		this.logic = logic;
+		this.serial = serial;
+		this.model = model;
+		this.sam = sam;
+		this.ptid = ptid;
+		this.plat = plat;
+		this.version = version;
+		this.mxr = mxr;
+		this.mxf = mxf;
+		this.VERFM = VERFM;
+	}
+
+	public Integer getLogic() {
+		return logic;
+	}
+
+	public void setLogic(Integer logic) {
+		this.logic = logic;
+	}
+
+	public String getSerial() {
+		return serial;
+	}
+
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public Integer getSam() {
+		return sam;
+	}
+
+	public void setSam(Integer sam) {
+		this.sam = sam;
+	}
+
+	public String getPtid() {
+		return ptid;
+	}
+
+	public void setPtid(String ptid) {
+		this.ptid = ptid;
+	}
+
+	public Integer getPlat() {
+		return plat;
+	}
+
+	public void setPlat(Integer plat) {
+		this.plat = plat;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public Integer getMxr() {
+		return mxr;
+	}
+
+	public void setMxr(Integer mxr) {
+		this.mxr = mxr;
+	}
+
+	public Integer getMxf() {
+		return mxf;
+	}
+
+	public void setMxf(Integer mxf) {
+		this.mxf = mxf;
+	}
+
+	public String getVERFM() {
+		return VERFM;
+	}
+
+	public void setVERFM(String vERFM) {
+		VERFM = vERFM;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((logic == null) ? 0 : logic.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Terminal other = (Terminal) obj;
+		if (logic == null) {
+			if (other.logic != null)
+				return false;
+		} else if (!logic.equals(other.logic))
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/com/laisleal/desafiotecnico/resources/TerminalResource.java
+++ src/main/java/com/laisleal/desafiotecnico/resources/TerminalResource.java
@@ -1,16 +1,32 @@
 package com.laisleal.desafiotecnico.resources;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.laisleal.desafiotecnico.domain.Terminal;
 
 @RestController
 @RequestMapping(value="/v1/Terminal")
 public class TerminalResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String testarRest() {
-		return "Testando Rest!";
+	public List<Terminal> testarRest() {
+		
+		Terminal terminal1 = new Terminal(11111, "123", "PWWIN", 0, "F04A2E4088B", 4, "8.00b3", 0, 16777216, "PWWIN");
+		Terminal terminal2 = new Terminal(22222, "111", "AAA", 0, "FFFFFFF", 4, "8.00b3", 0, 16777216, "PWWIN");
+		Terminal terminal3 = new Terminal(33333, "222", "BBB", 0, "GGGGGGG", 4, "8.00b3", 0, 16777216, "PWWIN");
+		
+		List<Terminal> list = new ArrayList();
+		list.add(terminal1);
+		list.add(terminal2);
+		list.add(terminal3);
+		
+		return list;
 	}
 
 }
